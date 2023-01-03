3c27085491b4cf877587b7c2a35296501e2b7539
yeonsoo-jeong <73508330+yeonsoo-jeong@users.noreply.github.com>
2023-01-03 12:34:58 +0900

    Add EncItemManager

---
+++ ShakaCallstackParser/EncItemManager.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShakaCallstackParser
+{
+    class EncItemManager
+    {
+        List<EncListItems> enc_items_ = new List<EncListItems>();
+
+        public EncItemManager()
+        {
+
+        }
+
+        public List<EncListItems> GetEncItems()
+        {
+            return enc_items_;
+        }
+
+        public void RemoveItems(List<EncListItems> items)
+        {
+            foreach (EncListItems item in items)
+            {
+                enc_items_.Remove(item);
+            }
+            ReorderEncListNumber();
+        }
+
+        public void RemoveFinishedItems()
+        {
+            for (int i = enc_items_.Count - 1; i >= 0; i--)
+            {
+                if (enc_items_[i].status == EncListItems.Status.success)
+                {
+                    enc_items_.RemoveAt(i);
+                }
+            }
+            ReorderEncListNumber();
+        }
+
+        public int GetToEncodeItemsNum()
+        {
+            int ret = 0;
+            for (int i = 0; i < enc_items_.Count; i++)
+            {
+                if (enc_items_[i].status != EncListItems.Status.success)
+                {
+                    ret++;
+                }
+            }
+
+            return ret;
+        }
+
+        public List<EncodeJob> GetToEncodeJobs()
+        {
+            List<EncodeJob> jobs = new List<EncodeJob>();
+            for (int i = 0; i < enc_items_.Count; i++)
+            {
+                if (enc_items_[i].status != EncListItems.Status.success)
+                {
+                    jobs.Add(new EncodeJob(i, enc_items_[i].path, EncodeManager.GetCoreNumFromCpuUsage(enc_items_[i].cpu_usage_selected)));
+                }
+            }
+
+            return jobs;
+        }
+
+        public void Refresh()
+        {
+            for (int i = 0; i < enc_items_.Count; i++)
+            {
+                if (enc_items_[i].status != EncListItems.Status.success)
+                {
+                    // for cancel & restart scenario
+                    enc_items_[i].note = "";
+                    enc_items_[i].progress = 0;
+                    enc_items_[i].status = EncListItems.Status.none;
+                }
+            }
+        }
+
+        public void DistinctAddFiles(string[] files)
+        {
+            if (files.Length >= 1)
+            {
+                foreach (string file in files)
+                {
+                    EncListItems item = new EncListItems();
+                    item.path = file;
+                    item.note = "core=" + Environment.ProcessorCount.ToString();
+                    item.cpu_usage = new List<string>()
+                        {
+                            "Full",
+                            "Half"
+                        };
+                    item.cpu_usage_selected = "Full";
+                    enc_items_.Add(item);
+                }
+
+                enc_items_ = enc_items_.Distinct(new EncListComparer()).ToList();
+                ReorderEncListNumber();
+            }
+        }
+
+        private void ReorderEncListNumber()
+        {
+            for (int i = 0; i < enc_items_.Count; i++)
+            {
+                enc_items_[i].number = i.ToString();
+            }
+        }
+    }
+
+    public class EncListItems
+    {
+        public enum Status
+        {
+            none,
+            success,
+            fail
+        }
+
+        public EncListItems()
+        {
+            progress = 0;
+            status = Status.none;
+        }
+
+        public string number { get; set; }
+        public string path { get; set; }
+        public int progress { get; set; }
+
+        public List<string> cpu_usage { get; set; }
+
+        public string cpu_usage_selected { get; set; }
+
+        public string note { get; set; }
+        public Status status { get; set; }
+    }
+
+    public class EncListComparer : IEqualityComparer<EncListItems>
+    {
+        public bool Equals(EncListItems x, EncListItems y)
+        {
+            return x.path == y.path;
+        }
+
+        public int GetHashCode(EncListItems obj)
+        {
+            if (obj == null)
+            {
+                return 0;
+            }
+
+            return obj.path == null ? 0 : obj.path.GetHashCode();
+        }
+    }
+}
--- ShakaCallstackParser/EncWindow.xaml.cs
+++ ShakaCallstackParser/EncWindow.xaml.cs
@@ -22,8 +22,8 @@ namespace ShakaCallstackParser
     public partial class EncWindow : Window
     {
         EncodeManager enc_manager_;
-        List<EncListItems> result = new List<EncListItems>();
-
+        EncItemManager enc_item_manager_;
+        
         const string kBtnLabelEncode = "Encode";
         const string kBtnLabelCancel = "Cancel";
 
@@ -35,48 +35,21 @@ namespace ShakaCallstackParser
 
         private void Init()
         {
-            ListView1.AllowDrop = true;
             EncodeManager.Callbacks callback = new EncodeManager.Callbacks(EncodeProgressChanged, 
                 EncodeFinished, AllEncodeFinished, SSIMCalculateFinished, AnalyzeFinished);
             enc_manager_ = new EncodeManager(callback);
-        }
-
-        private void ReorderEncListNumber()
-        {
-            for (int i = 0; i < result.Count; i++)
-            {
-                result[i].number = i.ToString();
-            }
-            ListView1.Items.Refresh();
+            enc_item_manager_ = new EncItemManager();
         }
 
         private void ListView1_OnDroped(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
-                //result.Clear();
-
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
                 if (files.Length >= 1)
                 {
-                    foreach (string file in files)
-                    {
-                        EncListItems item = new EncListItems();
-                        item.path = file;
-                        item.note = "core=" + Environment.ProcessorCount.ToString();
-                        item.cpu_usage = new List<string>()
-                        {
-                            "Full",
-                            "Half"
-                        };
-                        item.cpu_usage_selected = "Full";
-                        result.Add(item);
-                    }
-
-                    result = result.Distinct(new EncListComparer()).ToList();
-                    ReorderEncListNumber();
-
-                    ListView1.ItemsSource = result;
+                    enc_item_manager_.DistinctAddFiles(files);
+                    ListView1.ItemsSource = enc_item_manager_.GetEncItems();
                     ListView1.Items.Refresh();
                 }
             }
@@ -84,9 +57,10 @@ namespace ShakaCallstackParser
 
         private void TempWriteResult()
         {
-            for (int i = 0; i < result.Count; i++)
+            List<EncListItems> enc_items = enc_item_manager_.GetEncItems();
+            for (int i = 0; i < enc_items.Count; i++)
             {
-                Loger.Write("result[" + i + "]: index, number, combo, path=" + i + ", " + result[i].number + ", " + result[i].cpu_usage_selected + ", " + result[i].path);
+                Loger.Write("result[" + i + "]: index, number, combo, path=" + i + ", " + enc_items[i].number + ", " + enc_items[i].cpu_usage_selected + ", " + enc_items[i].path);
             }
             Loger.Write("");
         }
@@ -97,41 +71,9 @@ namespace ShakaCallstackParser
 
             if (Btn1.Content.ToString() == kBtnLabelEncode)
             {
-                if (ListView1.Items.Count > 0)
+                if (enc_item_manager_.GetToEncodeItemsNum() > 0)
                 {
-                    Btn1.IsEnabled = false;
-
-                    List<EncodeJob> jobs = new List<EncodeJob>();
-                    for (int i = 0; i < result.Count; i++)
-                    {
-                        if (result[i].status != EncListItems.Status.success)
-                        {
-                            if (result[i].status == EncListItems.Status.none)
-                            {
-                                // for cancel & restart scenario
-                                result[i].note = "";
-                                result[i].progress = 0;
-                            }
-
-                            //string path = ((EncListItems)ListView1.Items[i]).path;
-                            //jobs.Add(new EncodeJob(i, path));
-                            jobs.Add(new EncodeJob(i, result[i].path, EncodeManager.GetCoreNumFromCpuUsage(result[i].cpu_usage_selected)));
-                            
-                        }
-                    }
-                    enc_manager_.Start(jobs);
-                    Btn1.Content = kBtnLabelCancel;
-                    ListView1.IsEnabled = false;
-                    BtnRemoveDone.IsEnabled = false;
-                    ListView1.Items.Refresh();
-
-                    Task.Delay(1000).ContinueWith(_ =>
-                    {
-                        Dispatcher.Invoke(() =>
-                        {
-                            Btn1.IsEnabled = true;
-                        });
-                    });
+                    Start();
                 }
                 else
                 {
@@ -140,20 +82,7 @@ namespace ShakaCallstackParser
             }
             else if (Btn1.Content.ToString() == kBtnLabelCancel)
             {
-                Btn1.IsEnabled = false;
-
-                enc_manager_.OnEncodeCanceled();
-                Btn1.Content = kBtnLabelEncode;
-                ListView1.IsEnabled = true;
-                BtnRemoveDone.IsEnabled = true;
-
-                Task.Delay(1000).ContinueWith(_ =>
-                {
-                    Dispatcher.Invoke(() =>
-                    {
-                        Btn1.IsEnabled = true;
-                    });
-                });
+                Cancel();
             }
             else
             {
@@ -162,19 +91,61 @@ namespace ShakaCallstackParser
             }
         }
 
+        private void Start()
+        {
+            Btn1.IsEnabled = false;
+
+            enc_item_manager_.Refresh();
+            List<EncodeJob> jobs = enc_item_manager_.GetToEncodeJobs();
+
+            enc_manager_.Start(jobs);
+            Btn1.Content = kBtnLabelCancel;
+            ListView1.IsEnabled = false;
+            BtnRemoveDone.IsEnabled = false;
+            ListView1.Items.Refresh();
+
+            Task.Delay(1000).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Btn1.IsEnabled = true;
+                });
+            });
+        }
+
+        private void Cancel()
+        {
+            Btn1.IsEnabled = false;
+
+            enc_manager_.OnEncodeCanceled();
+            Btn1.Content = kBtnLabelEncode;
+            ListView1.IsEnabled = true;
+            BtnRemoveDone.IsEnabled = true;
+
+            Task.Delay(1000).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Btn1.IsEnabled = true;
+                });
+            });
+        }
+
         private void SSIMCalculateFinished(int index, int crf, double ssim)
         {
             Dispatcher.Invoke(() =>
             {
+                List<EncListItems> enc_items = enc_item_manager_.GetEncItems();
+
                 string strSSIM = Math.Round(ssim, 4).ToString();
                 string msg = crf.ToString() + ":" + strSSIM;
-                if (result[index].note.Length == 0)
+                if (enc_items[index].note.Length == 0)
                 {
-                    result[index].note = msg;
+                    enc_items[index].note = msg;
                 }
                 else
                 {
-                    result[index].note += ", " + msg;
+                    enc_items[index].note += ", " + msg;
                 }
                 ListView1.Items.Refresh();
             });
@@ -184,7 +155,9 @@ namespace ShakaCallstackParser
         {
             Dispatcher.Invoke(() =>
             {
-                result[index].note += " => " + crf.ToString();
+                List<EncListItems> enc_items = enc_item_manager_.GetEncItems();
+
+                enc_items[index].note += " => " + crf.ToString();
                 ListView1.Items.Refresh();
             });
         }
@@ -193,7 +166,9 @@ namespace ShakaCallstackParser
         {
             Dispatcher.Invoke(() => 
             {
-                result[index].progress = percentage;
+                List<EncListItems> enc_items = enc_item_manager_.GetEncItems();
+
+                enc_items[index].progress = percentage;
                 ListView1.Items.Refresh();
             });
         }
@@ -202,15 +177,17 @@ namespace ShakaCallstackParser
         {
             Dispatcher.Invoke(() =>
             {
+                List<EncListItems> enc_items = enc_item_manager_.GetEncItems();
+
                 if (result_code == 0)
                 {
-                    result[index].note = result[index].note + ", Success[" + result_code.ToString() + "]";
-                    result[index].status = EncListItems.Status.success;
+                    enc_items[index].note = enc_items[index].note + ", Success[" + result_code.ToString() + "]";
+                    enc_items[index].status = EncListItems.Status.success;
                 }
                 else
                 {
-                    result[index].note = result[index].note + ", Fail[" + result_code.ToString() + "]";
-                    result[index].status = EncListItems.Status.fail;
+                    enc_items[index].note = enc_items[index].note + ", Fail[" + result_code.ToString() + "]";
+                    enc_items[index].status = EncListItems.Status.fail;
                 }
                 ListView1.Items.Refresh();
             });
@@ -239,84 +216,19 @@ namespace ShakaCallstackParser
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             Loger.Write("e.RoutedEvent.Name=" + e.RoutedEvent.Name);
+            List<EncListItems> items = new List<EncListItems>();
             foreach (EncListItems item in ListView1.SelectedItems)
             {
-                result.Remove(item);
+                items.Add(item);
             }
-            ReorderEncListNumber();
+            enc_item_manager_.RemoveItems(items);
             ListView1.Items.Refresh();
         }
 
         private void BtnRemoveDone_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = result.Count - 1; i >= 0; i--)
-            {
-                if (result[i].status == EncListItems.Status.success)
-                {
-                    result.RemoveAt(i);
-                }
-            }
-            ReorderEncListNumber();
+            enc_item_manager_.RemoveFinishedItems();
             ListView1.Items.Refresh();
         }
     }
-
-    public class EncListItems
-    {
-        public enum Status
-        {
-            none,
-            success,
-            fail
-        }
-
-        public EncListItems()
-        {
-            progress = 0;
-            status = Status.none;
-        }
-
-        public string number { get; set; }
-        public string path { get; set; }
-        public int progress { get; set; }
-
-        public List<string> cpu_usage { get; set; }
-
-        public string cpu_usage_selected { get; set; }
-
-        public string note { get; set; }
-        public Status status { get; set; }
-    }
-
-    public class EncListComparer : IEqualityComparer<EncListItems>
-    {
-        public bool Equals(EncListItems x, EncListItems y)
-        {
-            return x.path == y.path;
-        }
-
-        public int GetHashCode(EncListItems obj)
-        {
-            if (obj == null)
-            {
-                return 0;
-            }
-
-            return obj.path == null ? 0 : obj.path.GetHashCode();
-        }
-    }
-
-    //public class SpeedComboBoxEntry
-    //{
-    //    private static readonly List<string> speed;
-
-    //    static SpeedComboBoxEntry()
-    //    {
-    //        speed = new List<string>();
-    //        speed.Add("full");
-    //        speed.Add("half");
-    //    }
-
-    //    public IEnumerable<string> SpeedSource => speed;
-    //}
 }
